793b5f2245aee0efb4db2992d50ce02d85f623dc
sam-19 <26563023+sam-19@users.noreply.github.com>
2021-04-06 13:54:03 +0300

    Production build config
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- webpack.build.js
+++ webpack.build.js
@@ -1,18 +1,24 @@
+const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.config.js')
-var ClosurePlugin = require('closure-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+const ASSET_PATH = process.env.ASSET_PATH || '/dist/'
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: ASSET_PATH,
+        filename: 'viewer.js',
+        chunkFilename: '[name].js?v=[contenthash]',
+        libraryTarget: 'umd'
+    },
     optimization: {
         minimize: true,
         minimizer: [
-            new ClosurePlugin({
-                mode: 'STANDARD',
-            }, {
-                languageOut: 'ECMASCRIPT_2015',
-            }),
+            new TerserPlugin(),
         ],
         concatenateModules: false,
     },
